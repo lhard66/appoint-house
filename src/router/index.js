7b5ed368efdf8f1b1d8fdec250bafb02ed0faf21
Liu TianQing <lhard@qq.com>
2018-03-11 22:25:06 +0800

    路由参数配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,13 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history',
   routes: [
     {
+      path: '/',
+      name: '默认页',
+      redirect: '/appoint/home'
+    }, {
       path: '/appoint/home',
       name: '预约首页',
       component: () => import('@/views/appointment/index.vue')
